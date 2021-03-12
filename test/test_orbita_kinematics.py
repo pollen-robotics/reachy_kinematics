0dba2b1cd049d70caf093ca844f59b55ca509910
Gaëlle Lannuzel <gaelle.lannuzel@gmail.com>
2021-03-12 13:00:25 +0100

    Add docstrings.

--- test/test_orbita_kinematics.py
+++ test/test_orbita_kinematics.py
@@ -1,3 +1,4 @@
+"""Unit tests for orbita_kinematics services."""
 import pytest
 
 import numpy as np
@@ -12,9 +13,11 @@ class TestOrbitaKinematics():
         cls.solver = orbita_kinematics.OrbitaKinSolver()
 
     def test_disk_order(self):
+        """Check that disks names are correct and in the correct order."""
         assert self.solver.disks_names == ['disk_top', 'disk_middle', 'disk_bottom']
 
-    def test_orbita_ik(self): 
+    def test_orbita_ik(self):
+        """Test disks positions returned by orbita inverse kinematics for the zero quaternion."""
         quat = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
         disks = self.solver.orbita_ik(quat)
         res_disks = list(disks.position)
@@ -23,6 +26,7 @@ class TestOrbitaKinematics():
         assert abs(res_disks[2] - (-1.04)) < 0.01
 
     def test_look_vector_to_quaternion(self):
+        """Test value of quaternion for a vector looking straight ahead."""
         point = Point(x=0.5, y=0.0, z=0.0)
         quat = self.solver.find_quaternion_transform(point)
         assert quat == Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
